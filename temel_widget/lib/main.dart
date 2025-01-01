cbed28e060382576d170efb87c51685a1e3c7df1
Onder Arslan <onder77@gmail.com>
2025-01-01 22:15:49 +0300

    If else condition added

--- temel_widget/lib/main.dart
+++ temel_widget/lib/main.dart
@@ -2,16 +2,45 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  String mesaj = "Merhaba ilk uygulamam!";
+
   runApp(MaterialApp(
-    home:Scaffold(
+    home: MyApp()
+  ));
+}
+
+class MyApp extends StatelessWidget
+{
+  String mesaj = "Merhaba ilk uygulamam!";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text(mesaj),
         backgroundColor: Colors.red,
       ),
       body: Center(
-        child: Text("This is body yo"),
+        child: ElevatedButton(
+          child: Text("Sonucu Gör"),
+          onPressed: (){
+            int puan = 15;
+            String sonuc = "";
+            if(puan >= 50)
+              sonuc = "Gecti";
+              else
+                sonuc = "Kaldı";
+            var alert = AlertDialog(
+              title: Text("Sınav Sonucu"),
+              content: Text(sonuc),
+            );
+            showDialog(context: context, builder: (BuildContext context)=> alert);
+          },
+
+
+        ),
+
       ),
-    )
-  ));
+    );
+  }
+  
 }
